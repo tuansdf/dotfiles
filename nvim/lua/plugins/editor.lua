b5869f17c8b0749a1ebb41d512a79a47fe774aa3
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-05-24 14:09:13 +0700

    add nvim

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,17 @@
+return {
+
+  -- add folding range to capabilities
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      capabilities = {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+          },
+        },
+      },
+    },
+  },
+}
