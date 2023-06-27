6a85a01fd92670f7ebaebee47b9fa0e3fc15816b
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-06-27 19:49:07 +0700

    add alacritty

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ git clone https://github.com/tuansdf/dotfiles
 echo "Symlinking"
 cd
 ln -s $HOME/dotfiles/.ideavimrc $HOME/.ideavimrc
-ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/alacritty $HOME/.config/alacritty
